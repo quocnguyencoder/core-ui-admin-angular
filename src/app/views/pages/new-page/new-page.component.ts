c7aaa617892e820b5d3a210f804ae2ce901d1280
Nguyen Quoc <quocnguyen612k@gmail.com>
2025-02-26 23:32:45 +0700

    first commit

---
+++ src/app/views/pages/new-page/new-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-page',
+  imports: [],
+  templateUrl: './new-page.component.html',
+  styleUrl: './new-page.component.scss'
+})
+export class NewPageComponent {
+
+}
---
+++ src/app/views/pages/routes.ts
@@ -0,0 +1,44 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '404',
+    loadComponent: () =>
+      import('./page404/page404.component').then((m) => m.Page404Component),
+    data: {
+      title: 'Page 404',
+    },
+  },
+  {
+    path: '500',
+    loadComponent: () =>
+      import('./page500/page500.component').then((m) => m.Page500Component),
+    data: {
+      title: 'Page 500',
+    },
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./login/login.component').then((m) => m.LoginComponent),
+    data: {
+      title: 'Login Page',
+    },
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./register/register.component').then((m) => m.RegisterComponent),
+    data: {
+      title: 'Register Page',
+    },
+  },
+  {
+    path: 'new-page',
+    loadComponent: () =>
+      import('./new-page/new-page.component').then((m) => m.NewPageComponent),
+    data: {
+      title: 'New Page',
+    },
+  },
+];
